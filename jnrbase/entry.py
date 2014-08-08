0623436d8ef03629ae28ea4ec9c98b86a46d1b69
James Rowe <jnrowe@gmail.com>
2014-08-08 19:33:51 +0100

    Add module entry point decorator

---
+++ jnrbase/entry.py
@@ -0,0 +1,30 @@
+#
+# coding=utf-8
+"""entry - Simple, lazy, module executing support"""
+# Copyright © 2014  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+def entry_point(f):
+    """Execute function when module is run directly
+
+    :param func f: Function to run
+    """
+    if f.__module__ == '__main__':
+        import sys
+        sys.exit(f())
+    else:
+        return f
---
+++ tests/test_entry.py
@@ -0,0 +1,37 @@
+#
+# coding=utf-8
+"""test_entry - Test entry point support"""
+# Copyright © 2014  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from expecter import expect
+
+from jnrbase.entry import entry_point
+
+
+def test_entrypoint():
+    def f():
+        return 42
+    f.__module__ = '__main__'
+    with expect.raises(SystemExit, '42'):
+        entry_point(f)
+
+
+def test_fallthrough():
+    @entry_point
+    def f():
+        return 42
+    expect(f()) == 42
