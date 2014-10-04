16a32d406d8f96a64577026f148a326050f905c7
James Rowe <jnrowe@gmail.com>
2014-10-04 11:19:47 +0100

    Add test for mangle_repr_type

---
+++ tests/test_compat.py
@@ -0,0 +1,29 @@
+#
+# coding=utf-8
+"""test_i18n - Test i18n setup functions"""
+# Copyright © 2014  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from jnrbase import compat
+
+
+def test_mangle_repr_type():
+    @compat.mangle_repr_type
+    class Test(object):
+        def __repr__(self):
+            return compat.text("test")
+    # This works on Python 2 or 3 by design
+    assert isinstance(repr(Test()), str)
