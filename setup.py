9c8c6c270cf44364a382db7d13d90bdeb3be078e
James Rowe <jnrowe@gmail.com>
2016-11-30 14:02:03 +0000

    Switch to running tests with nose2
    This cuts *eighty* percent off of the run time too!

--- setup.py
+++ setup.py
@@ -22,19 +22,7 @@ import glob
 import os
 
 from setuptools import setup
-from setuptools.command.test import test
 
-
-class PytestTest(test):
-    def finalize_options(self):
-        test.finalize_options(self)
-        self.test_args = ['tests/', ]
-        self.test_suite = True
-
-    def run_tests(self):
-        from sys import exit
-        from pytest import main
-        exit(main(self.test_args))
 
 # Hack to import _version file without importing jnrbase/__init__.py, its
 # purpose is to allow import without requiring dependencies at this point.
@@ -71,8 +59,6 @@ setup(
     packages=['jnrbase', ],
     install_requires=install_requires,
     extras_require=extras_require,
-    tests_require=['pytest'],
-    cmdclass={'test': PytestTest},
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
