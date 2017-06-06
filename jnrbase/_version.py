279def14f3d4cdc3a67fb5fca0f016da07e2f363
James Rowe <jnrowe@gmail.com>
2017-06-06 16:42:02 +0100

    Release 0.4.1

--- jnrbase/_version.py
+++ jnrbase/_version.py
@@ -1,10 +1,10 @@
-# This is jnrbase version 0.4.0 (2017-04-03)
-# pylint: disable=C0103, C0111, C0121, W0622
+# This is jnrbase version 0.4.1 (2017-06-06)
+# pylint: skip-file
 
-dotted = '0.4.0'
-libtool = '4:20'
-hex = 0x000400
-date = '2017-04-03'
-tuple = (0, 4, 0)
-dict = {'minor': 4, 'micro': 0, 'major': 0}
-web = 'jnrbase/0.4.0'
+dotted = '0.4.1'
+libtool = '4:21'
+hex = 0x000401
+date = '2017-06-06'
+tuple = (0, 4, 1)
+dict = {'micro': 1, 'minor': 4, 'major': 0}
+web = 'jnrbase/0.4.1'
