4931d539b951ddad330af7ef999db8b13e7c193d
James Rowe <jnrowe@gmail.com>
2017-09-26 00:17:42 +0100

    [QA] Bump copyright years
    This uses Greg's full amazing new updater, and you can colour me
    impressed.

--- jnrbase/pip_support.py
+++ jnrbase/pip_support.py
@@ -1,20 +1,21 @@
 #
 """pip_support - pip workarounds support."""
-# Copyright © 2014-2016  James Rowe <jnrowe@gmail.com>
+# Copyright © 2014-2017  James Rowe <jnrowe@gmail.com>
+#                        Nathan McGregor <nathan.mcgregor@astrium.eads.net>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of jnrbase.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# jnrbase is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# jnrbase is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License along with
+# jnrbase.  If not, see <http://www.gnu.org/licenses/>.
 
 # pip, both as a tool and a package manager, are not available on many of the
 # systems I use.  However, lots of Python users like to use it so we'll need to
