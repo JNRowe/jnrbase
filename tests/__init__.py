1bebfbd9d2c7dc20029d2d92bdb8e71d73cefe5c
James Rowe <jnrowe@gmail.com>
2017-11-14 20:45:45 +0000

    [QA] [Tr] Missing docstring in test package

---
+++ tests/__init__.py
@@ -0,0 +1,17 @@
+#
+"""tests - jnrbase testsuite."""
+# Copyright © 2014-2017  James Rowe <jnrowe@gmail.com>
+#
+# This file is part of jnrbase.
+#
+# jnrbase is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# jnrbase is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# jnrbase.  If not, see <http://www.gnu.org/licenses/>.
