e51ecd11eb6c96a47bda880dd5d946f193fa5c56
James Rowe <jnrowe@gmail.com>
2017-11-20 22:05:01 +0000

    Release 0.8.1

--- jnrbase/_version.py
+++ jnrbase/_version.py
@@ -1,10 +1,10 @@
-# This is jnrbase version 0.8.0 (2017-11-20)
+# This is jnrbase version 0.8.1 (2017-11-20)
 # pylint: skip-file
 
-dotted = '0.8.0'
-libtool = '8:20'
-hex = 0x000800
+dotted = '0.8.1'
+libtool = '8:21'
+hex = 0x000801
 date = '2017-11-20'
-tuple = (0, 8, 0)
-dict = {'major': 0, 'minor': 8, 'micro': 0}
-web = 'jnrbase/0.8.0'
+tuple = (0, 8, 1)
+dict = {'major': 0, 'minor': 8, 'micro': 1}
+web = 'jnrbase/0.8.1'
