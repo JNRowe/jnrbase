ce8e53d43f0f20b15469bd8f31017619637e5418
James Rowe <jnrowe@gmail.com>
2018-01-17 06:51:24 +0000

    [QA] Add missing tests for .cmdline
    These already existed, but were trapped on a branch.

---
+++ tests/test_cmdline.py
@@ -0,0 +1,38 @@
+#
+"""test_cmdline - Test command line functionality support"""
+# Copyright © 2014-2017  James Rowe <jnrowe@gmail.com>
+#
+# This file is part of jnrbase.
+#
+# jnrbase is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# jnrbase is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# jnrbase.  If not, see <http://www.gnu.org/licenses/>.
+
+from shutil import which
+
+from click.testing import CliRunner
+from pytest import mark, raises
+
+from jnrbase.cmdline import cli
+
+
+pytestmark = mark.skipif(not which('git'), reason='Requires git')
+
+
+@mark.parametrize('type_', ['fail', 'info', 'success', 'warn'])
+def test_messages(type_):
+    runner = CliRunner()
+    result = runner.invoke(cli, ['messages', type_, 'test str'])
+    if type_ == 'fail':
+        assert result.exit_code == 1
+    else:
+        assert result.exit_code == 0
+    assert 'test str' in result.output
