4ab688a92182f8a2d83479a3c24a499f1b583491
James Rowe <jnrowe@gmail.com>
2018-01-17 22:19:13 +0000

    [Tr] Remove unused import

--- jnrbase/pager.py
+++ jnrbase/pager.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License along with
 # jnrbase.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from subprocess import run
 from typing import Optional
 
