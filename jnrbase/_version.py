ed95994d258fc1de7b0246463d5f9286571d0796
James Rowe <jnrowe@gmail.com>
2018-11-24 10:56:35 +0000

    Release 1.1.1

--- jnrbase/_version.py
+++ jnrbase/_version.py
@@ -1,10 +1,10 @@
-# This is jnrbase version 1.1.0 (2018-11-21)
+# This is jnrbase version 1.1.1 (2018-11-24)
 # pylint: skip-file
 
-dotted = '1.1.0'
-libtool = '11:20'
-hex = 0x010100
-date = '2018-11-21'
-tuple = (1, 1, 0)
-dict = {'major': 1, 'minor': 1, 'micro': 0}
-web = 'jnrbase/1.1.0'
+dotted = '1.1.1'
+libtool = '11:21'
+hex = 0x010101
+date = '2018-11-24'
+tuple = (1, 1, 1)
+dict = {'major': 1, 'minor': 1, 'micro': 1}
+web = 'jnrbase/1.1.1'
