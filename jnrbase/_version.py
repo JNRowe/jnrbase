d2c1079b75018a0a0ab861ba6cb1138d7f9a8a5f
James Rowe <jnrowe@gmail.com>
2020-01-26 10:11:34 +0000

    Release 1.2.1

--- jnrbase/_version.py
+++ jnrbase/_version.py
@@ -1,10 +1,10 @@
-# This is jnrbase version 1.2.0 (2020-01-23)
+# This is jnrbase version 1.2.1 (2020-01-26)
 # pylint: skip-file
 
-dotted = '1.2.0'
-libtool = '12:20'
-hex = 0x010200
-date = '2020-01-23'
-tuple = (1, 2, 0)
-dict = {'major': 1, 'minor': 2, 'micro': 0}
-web = 'jnrbase/1.2.0'
+dotted = '1.2.1'
+libtool = '12:21'
+hex = 0x010201
+date = '2020-01-26'
+tuple = (1, 2, 1)
+dict = {'major': 1, 'minor': 2, 'micro': 1}
+web = 'jnrbase/1.2.1'
